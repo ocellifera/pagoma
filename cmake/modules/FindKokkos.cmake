6e69b2f0b4ec75f6b7257a5d8b407bfc829217e7
landinjm <landinjm@umich.edu>
2025-09-12 11:29:23 -0700

    temp

--- cmake/modules/FindKokkos.cmake
+++ cmake/modules/FindKokkos.cmake
@@ -2,21 +2,22 @@
 # code
 
 if(PAGOMA_EXTERNAL_PACKAGES_ONLY)
-  find_package(Kokkos 4.4.00 REQUIRED CONFIG)
+	find_package(Kokkos 4.4.00 REQUIRED CONFIG)
 elseif(PAGOMA_EMBEDDED_PACKAGES_ONLY)
 else()
-  find_package(Kokkos 4.4.00 CONFIG)
-  if(Kokkos_FOUND)
-    message(
-      STATUS
-      "Found Kokkos: ${Kokkos_DIR} (version \"${Kokkos_VERSION}\")"
-    )
-  else()
-    FetchContent_Declare(
-      Kokkos
-      GIT_REPOSITORY https://github.com/kokkos/kokkos.git
-      GIT_TAG 4.7.00
-      GIT_SHALLOW TRUE
-    )
-  endif()
+	find_package(Kokkos 4.4.00 CONFIG)
+	if(Kokkos_FOUND)
+		message(
+						STATUS
+						"Found Kokkos: ${Kokkos_DIR} (version \"${Kokkos_VERSION}\")"
+				)
+	else()
+		FetchContent_Declare(
+						Kokkos
+						GIT_REPOSITORY https://github.com/kokkos/kokkos.git
+						GIT_TAG 4.7.00
+						GIT_SHALLOW TRUE
+				)
+		FetchContent_MakeAvailable(Kokkos)
+	endif()
 endif()
